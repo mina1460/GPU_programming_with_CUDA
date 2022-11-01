ed82702dc7fee260a82af25ee8a11f00a78cb5ef
Mohamed Ayman <mohamedayman15069@aucegypt.edu>
2022-11-02 00:53:16 +0200

    add library header file

---
+++ assignment 2/library.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <assert.h>
+#include "time_helper.h"
+#include "matrix.h"
+#include "GPU_task1.h"
+
+#define A_ROWS 3000
+#define A_COLS 2500
+#define B_ROWS A_COLS
+#define B_COLS 3000
