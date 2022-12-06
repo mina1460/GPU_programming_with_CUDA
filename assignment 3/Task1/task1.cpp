de5445a976cce47e59c1ce01d0334cfac6e3565c
Mina Ashraf <mina146@aucegypt.edu>
2022-12-06 16:03:02 +0200

    task 1 and 3

--- assignment 3/Task1/task1.cpp
+++ assignment 3/Task1/task1.cpp
@@ -9,6 +9,18 @@ using namespace cimg_library;
 
 using namespace std;
 
+
+#define MAX_MASK_SIZE 9
+
+#define MAX_MASK_HEIGHT 3
+#define MAX_MASK_WIDTH 3
+
+#define O_TILE_WIDTH 9
+
+__constant__ float d_mask[MAX_MASK_SIZE];
+
+#define TILE_SIZE 8
+
 const float blur_kernel[3][3] = {
     {0.0625, 0.125, 0.0625},
     {0.125, 0.25, 0.125},
@@ -79,6 +91,110 @@ const string kernel_names[8] = {
     "bottom_sobel",
 };
 
+__global__ void convolution_2D_tiled_kernel(float *d_input, float *d_output, int width, int height, int mask_width) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    __shared__ float N_ds[TILE_SIZE][TILE_SIZE];
+    if (row < height && col < width) {
+        N_ds[threadIdx.y][threadIdx.x] = d_input[row * width + col];
+    } 
+    __syncthreads();
+
+    int tile_row_start = blockIdx.y * blockDim.y ;
+    int tile_row_end = (blockIdx.y+1) + blockDim.y;
+    int tile_col_start = blockIdx.x * blockDim.x ;
+    int tile_col_end = (blockIdx.x+1) + blockDim.x;
+
+    float pvalue = 0;
+    int row_start = row - (mask_width / 2);
+    int col_start = col - (mask_width / 2);
+
+    for(int i = 0; i < mask_width; i++) {
+        for(int j = 0; j < mask_width; j++) {
+            int cur_row = row_start + i;
+            int cur_col = col_start + j;
+            if (cur_row < 0){
+                cur_row = 0;
+            }  
+            else if(cur_row >= height){
+                cur_row = height - 1;
+            }
+            
+            if (cur_col < 0){
+                cur_col = 0;
+            }  
+            else if(cur_col >= width){
+                cur_col = width - 1;
+            }
+        
+            if(cur_row >= tile_row_start && cur_row < tile_row_end && cur_col >= tile_col_start && cur_col < tile_col_end) {
+                pvalue += N_ds[threadIdx.y + (cur_row - tile_row_start)][threadIdx.x + (cur_col - tile_col_start)] * d_mask[i * mask_width + j];
+            } else {
+                // uses general caching 
+                pvalue += d_input[cur_row * width + cur_col] * d_mask[i * mask_width + j];
+            }
+        }
+    }
+}
+
+__global__ void convolution_output_tiling_2D_kernel(float *d_input, float *d_output, int width, int height) {
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    int row_o = blockIdx.y*O_TILE_WIDTH + ty; 
+    int col_o = blockIdx.x*O_TILE_WIDTH + tx;
+    
+    int row_i = row_o - MAX_MASK_WIDTH/2; 
+    int col_i = col_o - MAX_MASK_WIDTH/2;
+    
+    __shared__ float N_ds[TILE_SIZE+MAX_MASK_WIDTH-1][TILE_SIZE+MAX_MASK_HEIGHT-1];
+    if(row_i < 0) row_i = 0;
+    if(row_i >= height) row_i = height - 1;
+    if(col_i < 0) col_i = 0;
+    if(col_i >= width) col_i = width - 1;
+
+    N_ds[ty][tx] = d_input[row_i*width+col_i];
+    __syncthreads();
+
+    float output = 0.0f;
+    if(ty < O_TILE_WIDTH && tx < O_TILE_WIDTH){
+        for(int i = 0; i < MAX_MASK_WIDTH; i++) { 
+            for(int j = 0; j < MAX_MASK_WIDTH; j++) {
+                output += d_mask[i][j] * N_ds[i+ty][j+tx]; 
+            }
+        }
+    
+        if(row_o < height && col_o < width){ 
+            data[row_o*width + col_o] = output;
+        } 
+    }
+}
+    
+    
+void GPU_apply_convolution_kernel(CImg<float> &h_image, const float *h_mask) {
+
+    float *d_image;
+    float *d_kernel;
+    float *d_result;
+    cuda_malloc((void **)&d_image, h_image.size() * sizeof(float));
+    // copy filter to symbol memory
+    cuda_malloc((void **)&d_result, h_image.size() * sizeof(float));
+
+    // copy image to device
+    cudaMemcpy(d_image, h_image.data(), image.size() * sizeof(float), cudaMemcpyHostToDevice);
+    // and copy filter to static memory
+    cudaMemcpyToSymbol(d_mask, h_mask, 9 * sizeof(float));
+
+
+    // set up the execution configuration
+    dim3 dimBlock(TILE_SIZE, TILE_SIZE);
+    dim3 dimGrid((h_image.width() + dimBlock.x - 1) / dimBlock.x, (h_image.height() + dimBlock.y - 1) / dimBlock.y);
+
+    
+
+
+}
+
 
 void apply_convolution_kernel(float* input_data, float* output_data, const float* kernel, int kernel_width, int kernel_height, int img_width, int img_height){
     int output_index, input_r, input_c, input_index, kernel_index;
@@ -137,16 +253,17 @@ int main(int argc, char *argv[])
 
     cout << choose_conv_kernel_msg << endl;
     int kernel_choice(1);
-    //cin >> kernel_choice;
+    cin >> kernel_choice;
+
 
     if (kernel_choice < 1 || kernel_choice > 8)
     {
         cout << "invalid kernel choice" << endl;
         return -2;
     }
-
+    kernel_choice -= 1;
     // get the kernel pointer
-    const float *kernel = kernels[kernel_choice - 1];
+    const float *kernel = kernels[kernel_choice];
 
     // open the image with cimg library
     CImg<float> img(img_path.c_str());
@@ -165,25 +282,20 @@ int main(int argc, char *argv[])
     float* orig_values = img.data();
     float* result_values = new float[width * height * depth];
     
-    for(int kernel_choice=7; kernel_choice >=0 ; kernel_choice--){
-        cout << "Applying kernel " << kernel_names[kernel_choice] << endl;
-        
-        for(int i=0; i < width * height * depth; i++){
-            result_values[i] = 0;
-        }
-        const float *kernel = kernels[kernel_choice];
-        // apply the convolution kernel
-        apply_convolution_kernel(orig_values, result_values, kernel, 3, 3, width, height);
-        
-        // create a new image to store the result
-        CImg<float> result_img(result_values, width, height, 1, depth);
-        string result_img_path = img_basename + "_" + kernel_names[kernel_choice] + ".jpeg";
-        result_img.save(result_img_path.c_str());
-        cout << "result image saved to " << result_img_path << endl;
-
+    cout << "Applying kernel " << kernel_names[kernel_choice] << endl;
+    
+    for(int i=0; i < width * height * depth; i++){
+        result_values[i] = 0;
     }
     
+    // apply the convolution kernel
+    apply_convolution_kernel(orig_values, result_values, kernel, 3, 3, width, height);
     
+    // create a new image to store the result
+    CImg<float> result_img(result_values, width, height, 1, depth);
+    string result_img_path = img_basename + "_" + kernel_names[kernel_choice] + ".jpeg";
+    result_img.save(result_img_path.c_str());
+    cout << "result image saved to " << result_img_path << endl;    
     
     delete[] result_values;
     return 0;
--- assignment 3/Task3/task3.cpp
+++ assignment 3/Task3/task3.cpp
@@ -8,14 +8,77 @@
 
 using namespace cimg_library;
 using namespace std;
+#define BLOCK_SIZE 32
 
-void compute_histogram(int32_t* input_values, int32_t* histogram, int img_width, int img_height, int num_of_hist_bins){
-    int pixel_value = 0;
+__global__ void privatized_interleaved_histogram(unsigned char *d_input, int32_t *d_histogram, int width, int height, int numBins) {
+
+    extern __shared__ int smem[];
+    
+    int bin_size = 256 / numBins;
+
+    // pixel coordinates
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    // grid dimensions
+    int n_of_threads_in_x = blockDim.x * gridDim.x;
+    int n_of_threads_in_y = blockDim.y * gridDim.y;
+
+    // linear thread index within 2D block
+    int tid = threadIdx.x + threadIdx.y * blockDim.x; 
+
+    if(tid < numBins) 
+        smem[i] = 0;
+    __syncthreads();
+    
+    for (int col = x; col < width; col += n_of_threads_in_x)
+    { 
+        for (int row = y; row < height; row += n_of_threads_in_y) { 
+            unsigned int bin_val = d_input[col + row * width] / bin_size;
+            if(bin_val >= numBins){
+                bin_val = numBins - 1;
+            }
+            atomicAdd(&smem[bin_val], 1);
+        }
+    }
+    __syncthreads();
+
+    if(tid < numBins)
+        atomicAdd(&d_histogram[i], smem[i]);
+}
+
+void GPU_compute_histogram(char* input_values, int32_t* h_histogram, int img_width, int img_height, int num_of_hist_bins){
+    unsigned char* d_input_values;
+    int32_t* d_histogram;
+    
+    cuda_malloc((void**)&d_input_values, img_width*img_height*sizeof(unsigned char));
+    cuda_memcpy(d_input_values, input_values, img_width*img_height*sizeof(unsigned char), cudaMemcpyHostToDevice);
+
+
+    cuda_malloc((void**)&d_histogram, num_of_hist_bins*sizeof(int32_t));
+    cuda_memset(d_histogram, 0, num_of_hist_bins*sizeof(int32_t));
+
+    // launch kernel with number of rows = img_height, number of columns = 1
+    dim3 block(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 grid(ceil(img_width/(float)BLOCK_SIZE), ceil(img_height/(float)BLOCK_SIZE));
+    privatized_interleaved_histogram<<<grid, block, num_of_hist_bins*sizeof(int32_t)>>>(d_input_values, d_histogram, img_width, img_height, num_of_hist_bins);
+
+    int32_t* h_histogram = (int32_t*)malloc(num_of_hist_bins*sizeof(int32_t));
+    cuda_memcpy(h_histogram, d_histogram, num_of_hist_bins*sizeof(int32_t), cudaMemcpyDeviceToHost);
+
+    cuda_free(d_input_values);
+    cuda_free(d_histogram);
+    
+}
+
+
+
+void compute_histogram(unsigned char* input_values, int32_t* histogram, int img_width, int img_height, int num_of_hist_bins){
+    unsigned char pixel_value = 0;
     int bin = 0;
     for(int i=0; i<img_height*img_width; i++){
         pixel_value = input_values[i];
-        // pixel_value = max(0, pixel_value);
-        // pixel_value = min(255, pixel_value);
+        
         bin = pixel_value / (256/num_of_hist_bins);
         if(bin >= num_of_hist_bins){
             // assign to last bin as they are the result of integer division 
@@ -27,7 +90,7 @@ void compute_histogram(int32_t* input_values, int32_t* histogram, int img_width,
 }
 
 void test_compute_histogram(){
-    int32_t input_test_data[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12, 13, 14, 15};
+    unsigned char input_test_data[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12, 13, 14, 15};
     int32_t histogram[4] = {0};
     compute_histogram(input_test_data, histogram, 4, 4, 4);
     for(int i=0; i<4; i++){
@@ -38,7 +101,7 @@ void test_compute_histogram(){
         
     }
     cout << "1: test_compute_histogram passed!!" << endl;
-    int32_t input_test_data2[16] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1};
+    unsigned char input_test_data2[16] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1};
     int32_t histogram2[4] = {0};
     compute_histogram(input_test_data2, histogram2, 4, 4, 4);
     if(histogram2[0] != 16 && histogram2[1] != 0 && histogram2[2] != 0 && histogram2[3] != 0){
@@ -49,6 +112,16 @@ void test_compute_histogram(){
     cout << endl;
 }
 
+bool compare(int32_t* cpu_results, int32_t* gpu_results, int size){
+    for(int i=0; i<size; i++){
+        if(cpu_results[i] != gpu_results[i]){
+            cout << "compare failed at index: " << i << endl;
+            cout <<"Cpu: " << cpu_results[i] << " Gpu: " << gpu_results[i] << endl;
+            return false;
+        }
+    }
+    return true;
+}
 
 int main(int argc, char* argv[]){
     if(argc != 2){
@@ -58,12 +131,12 @@ int main(int argc, char* argv[]){
     test_compute_histogram();
     
     string img_file_path = argv[1];
-    CImg<int32_t> img(img_file_path.c_str());
+    CImg<unsigned char> img(img_file_path.c_str());
     int img_width = img.width();
     int img_height = img.height();
     int img_channels = img.spectrum();
     int img_size = img_width * img_height * img_channels;
-    int32_t* img_values = img.data();
+    unsigned char* img_values = img.data();
     cout << "Image width: " << img_width << endl;
     cout << "Image height: " << img_height << endl;
     cout << "Image channels: " << img_channels << endl;
@@ -78,7 +151,15 @@ int main(int argc, char* argv[]){
     for(int i=0; i<num_bins; i++){
         cout << "Bin " << i << ": " << histogram[i] << endl;
     }
+    int32_t* gpu_histogram;
+    GPU_compute_histogram(img_values, gpu_histogram, img_width, img_height, num_bins);
 
+    if(compare(histogram, gpu_histogram, num_bins)){
+        cout << "GPU_compute_histogram passed!!" << endl;
+    }
+    else{
+        cout << "GPU_compute_histogram failed!!" << endl;
+    }
 
     return 0;
 }
